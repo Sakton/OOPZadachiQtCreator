431b50f50a49e70d21bad4ff284ff04ce2c0e330
RomanRazumov <sakton@mail.ru>
2020-10-29 22:50:42 +0300

    pr

--- Containers/Containers_As_Parameters/DynamicList/dynamiclist.cpp
+++ Containers/Containers_As_Parameters/DynamicList/dynamiclist.cpp
@@ -1,1 +1,34 @@
 #include "dynamiclist.h"
+
+bool DynamicList::Iterator::operator==( const DynamicList::Iterator& oit ) {
+  return elem_ == oit.elem_;
+}
+
+bool DynamicList::Iterator::operator!=( const DynamicList::Iterator& oit ) {
+  return !( *this == oit );
+}
+
+DynamicList::Iterator& DynamicList::Iterator::operator++( ) {
+  // FIXME проверки и throw
+  elem_ = elem_->next_;
+  return *this;
+}
+
+DynamicList::Iterator& DynamicList::Iterator::operator--( ) {
+  // FIXME проверки и throw
+  elem_ = elem_->prev_;
+  return *this;
+}
+
+DynamicList::value_type& DynamicList::Iterator::operator*( ) {
+  return elem_->item_;
+}
+
+// DynamicList::Iterator & DynamicList::Iterator::operator++(int)
+//{
+//}
+
+DynamicList::Node* DynamicList::Iterator::operator->( ) {
+  // FIXME проверки и throw
+  return elem_;
+}
--- Containers/Containers_As_Parameters/DynamicList/dynamiclist.h
+++ Containers/Containers_As_Parameters/DynamicList/dynamiclist.h
@@ -1,14 +1,51 @@
 #ifndef DYNAMICLIST_H
 #define DYNAMICLIST_H
+#include <cstdint>
+
+// классы исключений добавить
+
+class Iterator;
 
 class DynamicList {
  public:
   // определения типов
   using value_type = double;
   using size_type = size_t;
-  //итераторы
-  class Iterator;
 
+  // класс узел
+ private:
+  struct Node {
+    Node( const value_type& v )
+        : item_ { v }, next_ { nullptr }, prev_ { nullptr } {}
+    Node( ) : item_ { }, next_ { nullptr }, prev_ { nullptr } {}
+
+    value_type item_;
+    Node* next_;
+    Node* prev_;
+  };
+
+ public:
+  // класс итератор
+  class Iterator {
+   public:
+    friend class DynamicList;
+    // ctor
+    Iterator( ) : elem_ { nullptr } { };
+    Iterator( const Iterator& oit ) : elem_ { oit.elem_ } {}
+    // predicates
+    bool operator==( const Iterator& oit );
+    bool operator!=( const Iterator& oit );
+    // behavior
+    Iterator& operator++( );
+    Iterator& operator--( );
+    value_type& operator*( );
+    Node* operator->( );
+    Iterator& operator++( int );
+    Iterator& operator--( int );
+
+   private:
+    Node* elem_;
+  };
   // кострукторы/деструкторы/копирование/присваивание
   DynamicList( );
   DynamicList( const value_type& v, size_type n = 1 );
@@ -18,9 +55,15 @@ class DynamicList {
   DynamicList( DynamicList&& dl );
   DynamicList& operator=( const DynamicList& dl );
   DynamicList& operator=( DynamicList&& dl );
-  //
+  //итераторы
+  // Iterator
+
+ private:
+  int64_t count_;
+  Node* head_;
+  Node* tail_;
+  Iterator head_iterator_;
+  Iterator tail_iterator_;
 };
 
-class Iterator {};
-
-#endif // DYNAMICLIST_H
+#endif  // DYNAMICLIST_H
