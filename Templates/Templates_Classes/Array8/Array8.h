b901b72cac1da0aeb2451213bd2dc1d8707f0b68
RomanRazumov <sakton@mail.ru>
2020-11-10 21:29:56 +0300

    templ_ar8

--- Templates/Templates_Classes/Array8/Array8.h
+++ Templates/Templates_Classes/Array8/Array8.h
@@ -1,11 +1,13 @@
 #ifndef ARRAY8_H
 #define ARRAY8_H
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 // пусть у этого массива стратегия роста, тоесть всегда есть элементы
 
 template < typename T >
 class Array8 {
+ public:
   // type definition
   using type = T;
   using reference = T &;
@@ -16,7 +18,7 @@ class Array8 {
   using size_type = std::size_t;
 
   // ctor/operator/move/dtor
-  Array8( size_type n );
+  explicit Array8( size_type n = 5 );
   Array8( size_type n, type el );
   //  Array8( const_iterator beg, const_iterator end );
   ~Array8( );
@@ -37,16 +39,18 @@ class Array8 {
 
   // iterator
   iterator begin( );
-  const_iterator begin( ) const;
+  const_iterator cbegin( ) const;
   iterator end( );
-  const_iterator end( ) const;
+  const_iterator cend( ) const;
 
   // modificators
-  void isert( size_type idx );
+  void insert( size_type idx, const type &v );
   void remove( size_type idx );
 
  private:
   void upVolume( );
+  size_type newSize( size_type n );
+  void indexValid( size_type idx );
 
  private:
   type *elements_;
@@ -54,19 +58,19 @@ class Array8 {
   size_type capacity_;
 };
 
-#endif  // ARRAY8_H
-
 template < typename T >
 Array8< T >::Array8( Array8::size_type n )
-    : elements_ { new type[ 2 * n ] { T( ) } },
+    : elements_ { new type[ newSize( n ) ] { T( ) } },
       size_ { n },
-      capacity_ { 2 * n } {}
+      capacity_ { newSize( n ) } {}
 
 template < typename T >
 Array8< T >::Array8( Array8::size_type n, Array8::type el )
-    : elements_ { new type[ 2 * n ] { el } },
+    : elements_ { new type[ newSize( n ) ] { el } },
       size_ { n },
-      capacity_ { 2 * n } {}
+      capacity_ { newSize( n ) } {
+  std::fill( begin( ), end( ), el );
+}
 
 template < typename T >
 Array8< T >::~Array8( ) {
@@ -75,10 +79,10 @@ Array8< T >::~Array8( ) {
 
 template < typename T >
 Array8< T >::Array8( const Array8 &ar ) {
-  type *t = new type[ 2 * ar.size( ) ];
+  type *t = new type[ newSize( ar.size( ) ) ];
   std::copy( ar.cbegin( ), ar.cend( ), t );
   size_ = ar.size( );
-  capacity_ = 2 * ar.size( );
+  capacity_ = ar.capacity( );
   delete[] elements_;
   elements_ = t;
 }
@@ -95,7 +99,119 @@ Array8< T >::Array8( Array8 &&ar ) {
 template < typename T >
 Array8< T > &Array8< T >::operator=( const Array8 &ar ) {
   if ( this != &ar ) {
-    type *t = new type[ ar.size( ) * 2 ];
-    std::copy( );
+    type *t = new type[ newSize( ar.size( ) ) ];
+    std::copy( ar.begin( ), ar.end( ), t );
+    delete[] elements_;
+    elements_ = t;
+    size_ = ar.size_;
+    capacity_ = newSize( ar.size( ) );
   }
+  return *this;
 }
+
+template < typename T >
+Array8< T > &Array8< T >::operator=( Array8 &&ar ) {
+  if ( this != &ar ) {
+    delete[] elements_;
+    elements_ = ar.elements_;
+    ar.elements_ = nullptr;
+    size_ = ar.size_;
+    capacity_ = ar.capacity_;
+  }
+  return *this;
+}
+
+template < typename T >
+typename Array8< T >::size_type Array8< T >::size( ) const {
+  return size_;
+}
+
+template < typename T >
+typename Array8< T >::size_type Array8< T >::capacity( ) const {
+  return capacity_;
+}
+
+template < typename T >
+bool Array8< T >::isEmpty( ) {
+  return size_ == 0;
+}
+
+template < typename T >
+typename Array8< T >::reference Array8< T >::operator[](
+    Array8::size_type idx ) {
+  indexValid( idx );
+  return elements_[ idx ];
+}
+
+template < typename T >
+typename Array8< T >::const_reference Array8< T >::operator[](
+    Array8::size_type idx ) const {
+  indexValid( idx );
+  return elements_[ idx ];
+}
+
+template < typename T >
+typename Array8< T >::iterator Array8< T >::begin( ) {
+  return elements_;
+}
+
+template < typename T >
+typename Array8< T >::const_iterator Array8< T >::cbegin( ) const {
+  return elements_;
+}
+
+template < typename T >
+typename Array8< T >::iterator Array8< T >::end( ) {
+  return elements_ + size( );
+}
+
+template < typename T >
+typename Array8< T >::const_iterator Array8< T >::cend( ) const {
+  return elements_ + size( );
+}
+
+template < typename T >  // TODO requires testing
+void Array8< T >::insert( Array8::size_type idx, const type &v ) {
+  indexValid( idx );
+  if ( size( ) + 1 >= capacity( ) ) upVolume( );
+  if ( idx == size( ) ) {
+    ( *this )[ idx ] = v;
+  } else {
+    int i = size( ) - 1;
+    for ( ; i >= idx && i >= 0; --i ) {
+      ( *this )[ i + 1 ] = ( *this )[ i ];
+    }
+    ( *this )[ i + 1 ] = v;
+  }
+  ++size_;
+}
+
+template < typename T >  // TODO requires testing
+void Array8< T >::remove( Array8::size_type idx ) {
+  indexValid( idx );
+  for ( size_type i = idx; i < size( ) - 1; ++i ) {
+    ( *this )[ i ] = ( *this )[ i + 1 ];
+  }
+  --size_;
+}
+
+template < typename T >
+void Array8< T >::upVolume( ) {
+  type *t = new type[ newSize( size_ ) ];
+  std::copy( begin( ), end( ), t );
+  delete[] elements_;
+  elements_ = t;
+  capacity_ = newSize( size_ );
+}
+
+template < typename T >
+typename Array8< T >::size_type Array8< T >::newSize( Array8::size_type n ) {
+  return 2 * n;
+}
+
+template < typename T >
+void Array8< T >::indexValid( Array8::size_type idx ) {
+  if ( idx < 0 || size( ) < idx ) throw std::runtime_error( "OUT OF RANGE" );
+}
+
+#endif  // ARRAY8_H
--- Templates/Templates_Classes/Array8/main.cpp
+++ Templates/Templates_Classes/Array8/main.cpp
@@ -1,9 +1,69 @@
 #include <iostream>
 
-using namespace std;
+#include "Array8.h"
 
-int main()
-{
-  cout << "Hello World!" << endl;
-  return 0;
+int main( ) {
+  try {
+    //    Array8< int > a1( 5 );
+    //    std::copy( a1.cbegin( ), a1.cend( ),
+    //               std::ostream_iterator< int >( std::cout, " " ) );
+    //    std::cout << std::endl;
+    //    Array8< int > a2( 5, 5 );
+    //    std::copy( a2.cbegin( ), a2.cend( ),
+    //               std::ostream_iterator< int >( std::cout, " " ) );
+    //    std::cout << std::endl;
+    //    a1.isert( 0, 10 );
+    //    std::copy( a1.cbegin( ), a1.cend( ),
+    //               std::ostream_iterator< int >( std::cout, " " ) );
+    //    std::cout << std::endl;
+    //    a1.isert( a1.size( ) - 1, 20 );
+    //    std::copy( a1.cbegin( ), a1.cend( ),
+    //               std::ostream_iterator< int >( std::cout, " " ) );
+    //    std::cout << std::endl;
+
+    // std::cout << std::max( 10, 20 );
+
+    Array8< int > array;
+    array.insert( array.size( ), 10 );
+    array.insert( array.size( ), 20 );
+    //    array.isert( array.size( ), 30 );
+    //    array.isert( array.size( ), 40 );
+    //    array.isert( array.size( ), 50 );
+    //    array.isert( array.size( ), 60 );
+    //    array.isert( array.size( ), 70 );
+    array.insert( 0, 1 );
+    array.insert( 1, 2 );
+    array.insert( 2, 3 );
+    array.insert( 3, 4 );
+    array.insert( 4, 5 );
+    array.insert( 5, 6 );
+    array.insert( 6, 7 );
+    array.insert( 7, 8 );
+    array.insert( 8, 9 );
+    array.remove( 9 );
+    array.remove( 9 );
+    array.remove( 9 );
+    array.remove( 9 );
+    array.remove( 9 );
+    while ( array.size( ) ) {
+      array.remove( 0 );
+    }
+    array.insert( 0, 1 );
+    array.insert( 1, 2 );
+    array.insert( 2, 3 );
+    array.insert( 3, 4 );
+    array.insert( 4, 5 );
+    array.insert( 5, 6 );
+    array.insert( 6, 7 );
+    array.insert( 7, 8 );
+    array.insert( 8, 9 );
+    std::copy( array.cbegin( ), array.cend( ),
+               std::ostream_iterator< int >( std::cout, " " ) );
+    std::cout << std::endl;
+
+  } catch ( std::out_of_range &e ) {
+    std::cerr << e.what( ) << std::endl;
+  } catch ( ... ) {
+    std::cerr << "OTHER BAD" << std::endl;
+  }
 }
