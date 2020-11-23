7a2eb12f6ea60522880758bd7f1c93b04a66c350
sakton <sakton@mail.ru>
2020-11-23 15:20:02 +0300

    pr

--- STL/Container_Mapping/Mapping_Part_1/Map_21/main.cpp
+++ STL/Container_Mapping/Mapping_Part_1/Map_21/main.cpp
@@ -14,33 +14,7 @@ std::ostream &operator<<( std::ostream &ou, const Account_Bank &ab );
 
 class Pred {
  public:
-  // enum Ts { TsName, TsAcc, TsId };
-  // Pred( Ts ts = TsId ) : ts_ { ts } {}
   Pred( int data = 0 ) : data_ { data } { };
-
-  //  bool operator( )( const Account_Bank &a, const Account_Bank &b ) {
-  //    switch ( ts_ ) {
-  //      case TsName:
-  //        return a.name < b.name;
-  //      case TsAcc:
-  //        return a.acc < b.acc;
-  //      case TsId:
-  //        return a.id < b.id;
-  //    }
-  //  }
-
-  //  bool operator( )( const std::string &param, const Account_Bank &b ) {
-  //    switch ( ts_ ) {
-  //      case TsName:
-  //        return param < b.name;
-  //      case TsAcc:
-  //        return param < b.acc;
-  //      case TsId:
-  //        return false;
-  //    }
-  //    return false;
-  //  }
-
   bool operator( )( const std::string &a, const std::string &b ) const { return a > b; }
 
   bool operator( )( int ident, int x ) const { return ident < x; }
@@ -55,19 +29,65 @@ std::vector< Account_Bank > generate( );
 int main( ) {
   std::srand( std::time( nullptr ) );
   std::vector< Account_Bank > va = generate( );
-  std::map< std::string, Account_Bank, Pred > map_name;
+  for ( const auto &el : va ) {
+    std::cout << el;
+  }
 
-  auto it = std::find_if( va.cbegin( ), va.cend( ), []( const Account_Bank &a ) { return a.name < "30"; } );
+  std::cout << "/******************************************" << std::endl;
+  std::cout << "/******************************************" << std::endl;
+
+  std::map< std::string, Account_Bank, Pred > map_name;
+  auto fun = []( const Account_Bank &a ) { return a.name < "30"; };
+  auto it = std::find_if( va.cbegin( ), va.cend( ), fun );
   // поиск по имени и запись в мап
   // все остальные аналогичные
   while ( it != va.cend( ) ) {
     map_name.insert( std::make_pair( it->name, *it ) );
     ++it;
+    it = std::find_if( it, va.cend( ), fun );
   }
 
   for ( const auto &el : map_name ) {
     std::cout << el.second;
   }
+  //****************************
+  std::cout << "/******************************************" << std::endl;
+  std::map< std::string, Account_Bank, Pred > map_acc;
+  auto fun1 = []( const Account_Bank &a ) { return a.acc < "5000"; };
+
+  it = std::find_if( va.cbegin( ), va.cend( ), fun1 );
+  while ( it != va.cend( ) ) {
+    map_acc.insert( std::make_pair( it->name, *it ) );
+    ++it;
+    it = std::find_if( it, va.cend( ), fun1 );
+  }
+
+  for ( const auto &el : map_acc ) {
+    std::cout << el.second;
+  }
+
+  std::cout << "/**************** ID **********************" << std::endl;
+
+  auto fun2 = []( const Account_Bank &a ) { return a.id < 5000; };
+  std::map< int, Account_Bank, Pred > map_id;
+  it = std::find_if( va.cbegin( ), va.cend( ), fun2 );
+  while ( it != va.cend( ) ) {
+    map_id.insert( std::make_pair( it->id, *it ) );
+    ++it;
+    it = std::find_if( it, va.cend( ), fun2 );
+  }
+
+  for ( const auto &el : map_id ) {
+    std::cout << el.second;
+  }
+
+  std::cout << "/**************************************************************" << std::endl;
+  std::cout << "/**************************************************************" << std::endl;
+
+  // std::map< std::string, Account_Bank > mp1( va.begin( ), va.end( ) );  //без цикла не выйдет!!!!!!
+  //  for ( auto &el : mp1 ) {
+  //    std::cout << el.second << std::endl;
+  //  }
 }
 
 std::vector< Account_Bank > generate( ) {
