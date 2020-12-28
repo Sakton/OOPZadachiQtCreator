8189efe7d9345c0673fffe2e042bacaa0a571bf4
RomanRazumov <sakton@mail.ru>
2020-12-28 23:22:19 +0300

    pr

--- STL/STL_BOOK/AIDS/Templates/main.cpp
+++ STL/STL_BOOK/AIDS/Templates/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 template < typename T >
 void print( T arg ) {
@@ -11,4 +12,13 @@ void print( T first, Types... arg ) {
   print( arg... );
 }
 
-int main( ) { print( 1, "Hello", std::string( "World" ), 2.23F, "ersdfsdcasdcz" ); }
+template < typename C, typename... Idx >
+void printElems( const C& cont, Idx... index ) {
+  print( cont[ index ]... );
+}
+
+int main( ) {
+  print( 1, "Hello", std::string( "World" ), 2.23F, "ersdfsdcasdcz" );
+  std::vector< std::string > v { "one", "two", "tree", "for" };
+  printElems( v, 0, 0, 3, 3, 2, 1, 2, 3, 0, 1, 1, 1, 2, 3 );
+}
