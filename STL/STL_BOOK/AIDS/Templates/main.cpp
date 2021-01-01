9d6111d523c40f695c77a20440a857197f3922f2
RomanRazumov <sakton@mail.ru>
2021-01-01 19:29:22 +0300

    pr

--- STL/STL_BOOK/AIDS/Templates/main.cpp
+++ STL/STL_BOOK/AIDS/Templates/main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <vector>
 
-template < typename T >
-void print( T arg ) {
-  std::cout << arg << " ";
+// template < typename T >
+// void print( T arg ) {
+//  std::cout << arg << " ";
+//}
+
+// template < typename T, typename... Types >
+// void print( T first, Types... arg ) {
+//  print( first );
+//  print( arg... );
+//}
+
+template <typename T, typename... Type>
+void print(T first, Type... args) {
+  std::cout << first << std::endl;
+  if constexpr (sizeof...(args) > 0)
+    print(args...);
 }
 
-template < typename T, typename... Types >
-void print( T first, Types... arg ) {
-  print( first );
-  print( arg... );
+template <typename C, typename... Idx>
+void printElems(const C& cont, Idx... index) {
+  print(cont[index]...);
 }
 
-template < typename C, typename... Idx >
-void printElems( const C& cont, Idx... index ) {
-  print( cont[ index ]... );
-}
-
-int main( ) {
-  print( 1, "Hello", std::string( "World" ), 2.23F, "ersdfsdcasdcz" );
-  std::vector< std::string > v { "one", "two", "tree", "for" };
-  printElems( v, 0, 0, 3, 3, 2, 1, 2, 3, 0, 1, 1, 1, 2, 3 );
+int main() {
+  print(1, "Hello", std::string("World"), 2.23F, "ersdfsdcasdcz");
+  //  std::vector< std::string > v { "one", "two", "tree", "for" };
+  //  printElems( v, 0, 0, 3, 3, 2, 1, 2, 3, 0, 1, 1, 1, 2, 3 );
 }
